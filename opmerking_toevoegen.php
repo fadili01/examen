d6cd2ac9d6d2896b14b090b9cf40b498cd5e51e5
fadili01 <132888623+fadili01@users.noreply.github.com>
2025-04-07 23:12:02 +0200

    Add files via upload

--- opmerking_toevoegen.php
+++ opmerking_toevoegen.php
@@ -2,29 +2,30 @@
 include_once '../lessen/Les.php';
 
 $lesObj = new Les();
-$leerlingId = 1; // Dit zou uit de sessie komen, bijvoorbeeld: $_SESSION['leerling_id'];
+$instructeurId = 5; // Dit moet dynamisch komen, bijv. uit sessie of URL
 
-$lessen = $lesObj->getLessenVoorLeerling($leerlingId);
+$lessen = $lesObj->getLessenVoorInstructeur($instructeurId);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $lesId = $_POST['les_id'];
     $opmerking = $_POST['opmerking'];
 
-    if ($lesObj->voegOpmerkingToe($lesId, $opmerking, $leerlingId)) {
+    if ($lesObj->voegInstructeurOpmerkingToe($lesId, $opmerking, $instructeurId)) {
+
         echo "Opmerking toegevoegd!";
     } else {
-        echo "Fout bij toevoegen. Zorg ervoor dat je de opmerking voor een gekoppelde les toevoegt.";
+        echo "Fout bij toevoegen.";
     }
 }
 ?>
 
-<h2>Opmerking Toevoegen</h2>
+<h2>Les Opmerkingen Toevoegen</h2>
 
 <form method="POST">
-    <label for="les_id">Kies een les:</label>
+    <label for="les_id">Kies een leerling:</label>
     <select name="les_id" required>
         <?php foreach ($lessen as $les): ?>
-            <option value="<?php echo $les['id']; ?>"><?php echo $les['datum']; ?> - <?php echo $les['tijd']; ?></option>
+            <option value="<?php echo $les['id']; ?>"><?php echo $les['leerling_naam']; ?> - <?php echo $les['datum']; ?> - <?php echo $les['tijd']; ?></option>
         <?php endforeach; ?>
     </select><br>
 
